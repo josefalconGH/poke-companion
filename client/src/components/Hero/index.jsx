69c977a76d43ee22723b60d91c107c3b38bdba85
Jose Falcon <96765773+josefalconGH@users.noreply.github.com>
2024-08-16 13:37:59 -0500

    adds custom hero header styling

--- client/src/components/Hero/index.jsx
+++ client/src/components/Hero/index.jsx
@@ -1,11 +1,18 @@
 // Purpose: Hero component to render the hero section
 import React from "react";
 import pokeCompanionSM from "../../assets/images/poke-companion-sm.png";
+import pokeCompanionSMEdge from "../../assets/images/poke-companion-sm-edge.png";
 import pokeCompanionMD from "../../assets/images/poke-companion-md.png";
 
 const Hero = () => {
   return (
     <div className="hero-container">
+      <div
+        className="hero-background"
+        style={{
+          backgroundImage: `url(${pokeCompanionSMEdge})`,
+        }}
+      />
       <img
         src={pokeCompanionSM}
         alt="poke-companion-sm"
@@ -15,4 +22,5 @@ const Hero = () => {
   );
 };
 
+// export the Hero component
 export default Hero;
