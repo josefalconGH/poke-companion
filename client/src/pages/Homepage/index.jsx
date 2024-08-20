0849f3e1fedf8c054e73893795cc6608184af0fe
Jose Falcon <96765773+josefalconGH@users.noreply.github.com>
2024-08-20 18:14:22 -0500

    adds aria-labels to homepage hrefs

--- client/src/pages/Homepage/index.jsx
+++ client/src/pages/Homepage/index.jsx
@@ -4,27 +4,68 @@ import "./style.css";
 
 export default function Homepage() {
   return (
-    <main>
-      <h1 className="header-container font-extrabold flex justify-center text-center m-2 px-2">
-        Pokémon Companion - Updates &amp; Development
-      </h1>
-      <div className="panel-home m-4">
-        <p className="pb-2">
-          <span className="font-bold">Welcome to the PokéCompanion!</span> This
-          application is currently under development and will be updated with
-          new features/functionalities as they are developed.
-        </p>
-        <p className="pb-2">
-          Current planned features include: <a href="">Pokédex entries</a>,{" "}
-          <a href="">Data</a> for (types, evo chains, moves, items, locations),{" "}
-          <a href="">Coronation Tournament</a>, <a href="">Video Game</a>{" "}
-          information and much more!
-        </p>
-        <p>
-          Please check back for updates and feel free to reach out to the
-          developer <a href="https://www.instagram.com/josefalconig/">here</a>.
-        </p>
-      </div>
-    </main>
+    <div className="bg-gray-100 p-4 rounded-lg shadow-lg">
+      <main>
+        <header>
+          <h1 className="header-container font-extrabold flex justify-center text-center m-2 px-2">
+            Pokémon Companion - Updates &amp; Development
+          </h1>
+        </header>
+        <section className="panel-home m-4">
+          <p className="pb-2">
+            <span className="font-bold">Welcome to the PokéCompanion!</span>{" "}
+            This application is currently under development and will be updated
+            with new features and functionalities as they are developed.
+          </p>
+          <p className="pb-2">
+            Current planned features include:{" "}
+            <a
+              href="#"
+              className="hover:underline"
+              aria-label="Pokédex entries"
+            >
+              Pokédex entries
+            </a>
+            ,{" "}
+            <a
+              href="#"
+              className="hover:underline"
+              aria-label="Data for types, evolution chains, moves, items, locations"
+            >
+              Data
+            </a>{" "}
+            for (types, evo chains, moves, items, locations),{" "}
+            <a
+              href="#"
+              className="hover:underline"
+              aria-label="Coronation Tournament"
+            >
+              Coronation Tournament
+            </a>
+            ,{" "}
+            <a
+              href="#"
+              className="hover:underline"
+              aria-label="Video Game information"
+            >
+              Video Game
+            </a>{" "}
+            information and much more!
+          </p>
+          <p>
+            Please check back for updates and feel free to reach out to the
+            developer{" "}
+            <a
+              href="https://www.instagram.com/josefalconig/"
+              className="hover:underline"
+              aria-label="Developer's Instagram"
+            >
+              here
+            </a>
+            .
+          </p>
+        </section>
+      </main>
+    </div>
   );
 }
