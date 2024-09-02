5d3413bad7c781a1bde7fc2b92183dc6d83ee99a
Jose Falcon <96765773+josefalconGH@users.noreply.github.com>
2024-09-02 17:37:58 -0500

    adds new li elements, improves SEO

--- client/src/components/Dropdowns/DataDropdown.jsx
+++ client/src/components/Dropdowns/DataDropdown.jsx
@@ -2,17 +2,55 @@ import React from "react";
 import "./style.css";
 
 export default function DataDropdown({ isVisible }) {
-  if (!isVisible) return null; // Do not render if not visible
+  if (!isVisible) return null;
 
   return (
-    <ul className="dropdown-menu">
-      <li className="dropdown-item">Pokédex</li>
-      <li className="dropdown-item">Moves</li>
-      <li className="dropdown-item">Type Chart</li>
-      <li className="dropdown-item">Abilities</li>
-      <li className="dropdown-item">Items</li>
-      <li className="dropdown-item">Evo Chain</li>
-      <li className="dropdown-item">Locations</li>
+    <ul className="dropdown-menu" role="menu">
+      <li className="dropdown-item" role="menuitem">
+        <a href="/pokedex" title="View Pokédex">
+          Pokédex
+        </a>
+      </li>
+      <li className="dropdown-item" role="menuitem">
+        <a href="/moves" title="View Moves">
+          Moves
+        </a>
+      </li>
+      <li className="dropdown-item" role="menuitem">
+        <a href="/type-chart" title="View Type Chart">
+          Type Chart
+        </a>
+      </li>
+      <li className="dropdown-item" role="menuitem">
+        <a href="/abilities" title="View Abilities">
+          Abilities
+        </a>
+      </li>
+      <li className="dropdown-item" role="menuitem">
+        <a href="/items" title="View Items">
+          Items
+        </a>
+      </li>
+      <li className="dropdown-item" role="menuitem">
+        <a href="/evo-chain" title="View Evolution Chain">
+          Evo Chain
+        </a>
+      </li>
+      <li className="dropdown-item" role="menuitem">
+        <a href="/locations" title="View Locations">
+          Locations
+        </a>
+      </li>
+      <li className="dropdown-item" role="menuitem">
+        <a href="/training" title="View Training (EVs)">
+          Training (EVs)
+        </a>
+      </li>
+      <li className="dropdown-item" role="menuitem">
+        <a href="/breeding" title="View Breeding">
+          Breeding
+        </a>
+      </li>
     </ul>
   );
 }
