cee60e7f66bc51c764cd3323528e408f3e25af91
Jose Falcon <96765773+josefalconGH@users.noreply.github.com>
2024-09-02 02:58:44 -0500

    adjusts navbar item styling

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -6,9 +6,14 @@ import trainer from "../../assets/icons/trainer.svg";
 
 export default function NavBar() {
   const [searchActive, setSearchActive] = useState(false);
+  const [activeItem, setActiveItem] = useState(null);
 
   const handleSearchClick = () => {
     setSearchActive(!searchActive);
+  };
+
+  const handleItemClick = (item) => {
+    setActiveItem(item);
   };
 
   return (
@@ -16,26 +21,44 @@ export default function NavBar() {
       <div className="navbar-container">
         {!searchActive && (
           <ul className="navbar-menu">
-            <li className="navbar-item">
-              <a href="#" className="navbar-menu-heading" title="Data">
+            <li
+              className={`navbar-item ${activeItem === "Data" ? "active" : ""}`}
+              onClick={() => handleItemClick("Data")}
+            >
+              <a className="navbar-menu-heading" title="Data">
                 <img src={pokedex} alt="Data" className="navbar-icon" />
                 <span className="navbar-text">Data</span>
               </a>
             </li>
-            <li className="navbar-item">
-              <a href="#" className="navbar-menu-heading" title="Tournament">
-                <img src={crown} alt="Tournament" className="navbar-icon" />
-                <span className="navbar-text">Tournament</span>
+            <li
+              className={`navbar-item ${
+                activeItem === "World CS" ? "active" : ""
+              }`}
+              onClick={() => handleItemClick("World CS")}
+            >
+              <a className="navbar-menu-heading" title="World CS">
+                <img src={crown} alt="World CS" className="navbar-icon" />
+                <span className="navbar-text">World CS</span>
               </a>
             </li>
-            <li className="navbar-item">
-              <a href="#" className="navbar-menu-heading" title="Games">
+            <li
+              className={`navbar-item ${
+                activeItem === "Games" ? "active" : ""
+              }`}
+              onClick={() => handleItemClick("Games")}
+            >
+              <a className="navbar-menu-heading" title="Games">
                 <img src={game} alt="Games" className="navbar-icon" />
                 <span className="navbar-text">Games</span>
               </a>
             </li>
-            <li className="navbar-item">
-              <a href="#" className="navbar-menu-heading" title="Login">
+            <li
+              className={`navbar-item ${
+                activeItem === "Login" ? "active" : ""
+              }`}
+              onClick={() => handleItemClick("Login")}
+            >
+              <a className="navbar-menu-heading" title="Login">
                 <img src={trainer} alt="Login/Signup" className="navbar-icon" />
                 <span className="navbar-text">Login</span>
               </a>
