3f8cbf85c02a1235a01a337ef9e6b2a1ea5148ba
Jose Falcon <96765773+josefalconGH@users.noreply.github.com>
2024-09-02 17:55:20 -0500

    adds dropdown menu for World CS nav-item

---
+++ client/src/components/Dropdowns/WorldCSDropdown.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.css";
+
+export default function WorldCSDropdown({ isVisible }) {
+  if (!isVisible) return null;
+
+  return (
+    <ul className="dropdown-menu" role="menu">
+      <li className="dropdown-item" role="menuitem">
+        <a href="#" title="World Coronation Series">
+          World Coronation Series
+        </a>
+      </li>
+      <li className="dropdown-item" role="menuitem">
+        <a href="#" title="Rankings">
+          Rankings
+        </a>
+      </li>
+      <li className="dropdown-item" role="menuitem">
+        <a href="#" title="Rules">
+          Rules
+        </a>
+      </li>
+    </ul>
+  );
+}
