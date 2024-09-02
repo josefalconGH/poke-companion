7ae64e9280b58a8540e0ce238a743ce91590faa2
Jose Falcon <96765773+josefalconGH@users.noreply.github.com>
2024-09-02 00:01:09 -0500

    adds media query for hero-image

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     <footer className="bg-gradient-to-b from-neutral-700 to-neutral-950 text-white p-2">
       <div className="footer-custom text-left mb-2 ml-2">
         <a
-          href="/about#privacy"
+          href="#"
           className="font-bold text-zinc-100 hover:text-amber-200 underline inline-block mb-2"
         >
           Privacy Policy
