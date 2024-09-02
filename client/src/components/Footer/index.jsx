29f80006461536cc9fa49d6727ca2faf19d263f8
Jose Falcon <96765773+josefalconGH@users.noreply.github.com>
2024-09-02 02:19:07 -0500

    removes Tailwind CSS styling from footer/adds custom styling

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,18 +1,15 @@
 // Purpose: Footer component to render the footer
 export default function Footer() {
   return (
-    <footer className="bg-gradient-to-b from-neutral-700 to-neutral-950 text-white p-2">
-      <div className="footer-custom text-left mb-2 ml-2">
-        <a
-          href="#"
-          className="font-bold text-zinc-100 hover:text-amber-200 underline inline-block mb-2"
-        >
+    <footer className="footer">
+      <div className="footer-container">
+        <a href="#" className="footer-item">
           Privacy Policy
         </a>
-        <p className="footer-font font-semibold text-sm">
+        <p className="footer-font">
           All content & design &copy; PokéCompanion 2024.
         </p>
-        <p className="footer-font font-semibold text-sm">
+        <p className="footer-font">
           Pokémon images & names &copy; 1995-2024 Game Freak.
         </p>
       </div>
