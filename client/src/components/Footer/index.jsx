2b560fc988b76423faaf0e122f4dfdc042b71fb7
Jose Falcon <96765773+josefalconGH@users.noreply.github.com>
2024-09-03 17:23:08 -0500

    updates footer component to improve SEO

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -3,13 +3,23 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-container">
-        <a href="#" className="footer-item">
-          Privacy Policy
+        <a
+          href="#"
+          className="footer-item"
+          aria-label="Frequently Asked Questions"
+        >
+          Frequently Asked Questions
         </a>
-        <p className="footer-font">
+        <p
+          className="footer-font"
+          aria-label="All content and design copyright"
+        >
           All content & design &copy; PokéCompanion 2024.
         </p>
-        <p className="footer-font">
+        <p
+          className="footer-font"
+          aria-label="Pokemon images and names copyright"
+        >
           Pokémon images & names &copy; 1995-2024 Game Freak.
         </p>
       </div>
