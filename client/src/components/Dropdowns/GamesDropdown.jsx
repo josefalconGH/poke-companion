3717f2f70cc08f2477a5e4b940db885439cf71a5
Jose Falcon <96765773+josefalconGH@users.noreply.github.com>
2024-09-02 19:59:33 -0500

    updates dropdown-item names

--- client/src/components/Dropdowns/GamesDropdown.jsx
+++ client/src/components/Dropdowns/GamesDropdown.jsx
@@ -7,47 +7,47 @@ export default function GamesDropdown({ isVisible }) {
     <ul className="dropdown-menu" role="menu">
       <li className="dropdown-item" role="menuitem">
         <a href="#" title="Generation 9 (ScVi)">
-          Generation 9
+          Generation 9 (ScVi)
         </a>
       </li>
       <li className="dropdown-item" role="menuitem">
         <a href="#" title="Generation 8 (SwSh)">
-          Generation 8
+          Generation 8 (SwSh)
         </a>
       </li>
       <li className="dropdown-item" role="menuitem">
         <a href="#" title="Generation 7 (SuMo-USUM)">
-          Generation 7
+          Generation 7 (SuMo-USUM)
         </a>
       </li>
       <li className="dropdown-item" role="menuitem">
         <a href="#" title="Generation 6 (XY-ORAS)">
-          Generation 6
+          Generation 6 (XY-ORAS)
         </a>
       </li>
       <li className="dropdown-item" role="menuitem">
         <a href="#" title="Generation 5 (BW-B2W2)">
-          Generation 5
+          Generation 5 (BW-B2W2)
         </a>
       </li>
       <li className="dropdown-item" role="menuitem">
         <a href="#" title="Generation 4 (DP-Pt-HGSS)">
-          Generation 4
+          Generation 4 (DP-Pt-HGSS)
         </a>
       </li>
       <li className="dropdown-item" role="menuitem">
         <a href="#" title="Generation 3 (RS-E-FRLG)">
-          Generation 3
+          Generation 3 (RS-E-FRLG)
         </a>
       </li>
       <li className="dropdown-item" role="menuitem">
         <a href="#" title="Generation 2 (GS-C)">
-          Generation 2
+          Generation 2 (GS-C)
         </a>
       </li>
       <li className="dropdown-item" role="menuitem">
         <a href="#" title="Generation 1 (RB-Y)">
-          Generation 1
+          Generation 1 (RB-Y)
         </a>
       </li>
     </ul>
