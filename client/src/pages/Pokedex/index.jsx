8f1cecc11f0205a84a90d565720c559dba8f80da
Jose Falcon <96765773+josefalconGH@users.noreply.github.com>
2024-09-04 15:34:21 -0500

    adds Pokédex panel containing information for user

--- client/src/pages/Pokedex/index.jsx
+++ client/src/pages/Pokedex/index.jsx
@@ -8,6 +8,34 @@ export default function Pokedex() {
       <header>
         <h1 className="header">Pokémon Companion - Pokédex</h1>
       </header>
+      <section className="panel-pokedex">
+        <p className="padding-bottom">
+          The{" "}
+          <span className="panel-pokedex-span">PokémonCompanion Pokédex</span>{" "}
+          contains information on all Pokémon species from the Pokémon series.
+          Inside of the Pokédex, you can find various stats and information on
+          each Pokémon. With more than 1,000 Pokémon species, click on a
+          Pokémon's name to get a in-depth look at it's Pokédex data, including
+          descriptions and sprites from various games, as well as the Pokémon's
+          abilities, evolution lines, moves and more!
+        </p>
+        <p className="padding-bottom">
+          In addition,{" "}
+          <span className="panel-pokedex-span">
+            you can navigate the Pokédex by{" "}
+            <span href="" className="underline">
+              type
+            </span>{" "}
+            or{" "}
+            <span href="" className="underline">
+              generation
+            </span>{" "}
+            the Pokémon was introduced in.
+          </span>{" "}
+          This allows you to filter the Pokédex to showcase Pokémon of the same
+          type or from the same generation.
+        </p>
+      </section>
     </main>
   );
 }
