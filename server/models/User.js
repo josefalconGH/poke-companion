b936d06140d98b2913d8d15f8b96cf25b70d3c18
Jose Falcon <96765773+josefalconGH@users.noreply.github.com>
2024-09-05 16:26:55 -0500

    updates ES module syntax

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 // Purpose: User model
-const { Schema, model } = require("mongoose");
+import { Schema, model } from "mongoose";
 
 // create user schema
 const userSchema = new Schema({
@@ -21,23 +21,6 @@ const userSchema = new Schema({
     trim: true,
     minlength: 5,
   },
-  coronation_points: {
-    type: Number,
-    default: 0,
-  },
-  favorite_pokemon: {
-    type: Schema.Types.ObjectId,
-    ref: "Pokemon", // Reference to the Pokemon model
-    default: null,
-  },
-  favorite_type: {
-    type: String,
-    default: null,
-  },
-  favorite_game_versions: {
-    type: [String], // List of game versions, e.g., "firered", "red"
-    default: [],
-  },
   created_at: {
     type: Date,
     default: Date.now,
@@ -45,5 +28,4 @@ const userSchema = new Schema({
 });
 
 // create user model and export
-const User = model("User", userSchema);
-module.exports = User;
+export default model("User", userSchema);
--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,6 @@
 // Purpose: Export all models in one place
 import Pokedex from "./Pokedex.js";
+import User from "./User.js";
 
 // Export all models using ES module syntax
-export default {
-  Pokedex,
-};
+export { Pokedex, User };
