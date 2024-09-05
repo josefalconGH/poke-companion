958302ee2887ff52ca82c6de47eefe16b593a3d9
Jose Falcon <96765773+josefalconGH@users.noreply.github.com>
2024-09-05 16:08:52 -0500

    adds new models, adds new deps

--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,7 @@
 // Purpose: Export all models in one place
-const User = require("./User");
-const Pokemon = require("./Pokemon");
-const TypeEffectiveness = require("./TypeEffectiveness");
-const Moves = require("./Moves");
-const Items = require("./Items");
+import Pokedex from "./Pokedex.js";
 
-// export all models
-module.exports = {
-  User,
-  Pokemon,
-  TypeEffectiveness,
-  Moves,
-  Items,
+// Export all models using ES module syntax
+export default {
+  Pokedex,
 };
