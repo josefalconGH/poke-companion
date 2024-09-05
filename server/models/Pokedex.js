05e24f9cc1f10d0133917a1c9e81e1bcb032f446
Jose Falcon <96765773+josefalconGH@users.noreply.github.com>
2024-09-05 16:50:43 -0500

    updates Pokedex collection name in MongoDB

--- server/models/Pokedex.js
+++ server/models/Pokedex.js
@@ -2,72 +2,75 @@
 import { Schema, model } from "mongoose";
 
 // create pokemon schema
-const pokedexSchema = new Schema({
-  id: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  sprite: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  species: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  type: {
-    type: [String],
-    required: true,
-  },
-  abilities: {
-    type: [String],
-    required: true,
-  },
-  hp: {
-    type: Number,
-    required: true,
-  },
-  attack: {
-    type: Number,
-    required: true,
-  },
-  defense: {
-    type: Number,
-    required: true,
-  },
-  special_attack: {
-    type: Number,
-    required: true,
-  },
-  special_defense: {
-    type: Number,
-    required: true,
-  },
-  speed: {
-    type: Number,
-    required: true,
-  },
-  total: {
-    type: Number,
-    required: true,
-  },
-  appears_in: [
-    {
-      game_version: {
-        type: String,
-        required: true,
-      },
+const pokedexSchema = new Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+      unique: true,
     },
-  ],
-});
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    sprite: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    species: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    type: {
+      type: [String],
+      required: true,
+    },
+    abilities: {
+      type: [String],
+      required: true,
+    },
+    hp: {
+      type: Number,
+      required: true,
+    },
+    attack: {
+      type: Number,
+      required: true,
+    },
+    defense: {
+      type: Number,
+      required: true,
+    },
+    special_attack: {
+      type: Number,
+      required: true,
+    },
+    special_defense: {
+      type: Number,
+      required: true,
+    },
+    speed: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    appears_in: [
+      {
+        game_version: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  { collection: "pokedex" }
+);
 
 // export pokemon model using ES module syntax
 export default model("Pokedex", pokedexSchema);
