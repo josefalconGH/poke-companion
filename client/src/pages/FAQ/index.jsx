37c034c5987c5326d6efe91ee6e695f960915151
Jose Falcon <96765773+josefalconGH@users.noreply.github.com>
2024-09-08 13:38:07 -0500

    adds rough faq page and styling

---
+++ client/src/pages/FAQ/index.jsx
@@ -0,0 +1,21 @@
+// Purpose: Pokémon component to render the homepage
+import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
+import "./style.css";
+
+export default function Pokedex() {
+  return (
+    <main className="main-container">
+      <Helmet>
+        <title>Pokémon - </title>
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <link rel="canonical" href="https://poke-companion.com/faq" />
+      </Helmet>
+      <header>
+        <h1 className="header">Frequently Asked Questions</h1>
+      </header>
+      <section className="panel-faq"></section>
+    </main>
+  );
+}
