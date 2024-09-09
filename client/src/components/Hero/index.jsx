812bb70cbd76adf664669c73bf241ccb66387421
Jose Falcon <96765773+josefalconGH@users.noreply.github.com>
2024-09-08 19:24:13 -0500

    increases lighthouse properly sized images score

--- client/src/components/Hero/index.jsx
+++ client/src/components/Hero/index.jsx
@@ -1,6 +1,8 @@
 // Purpose: Hero component to render the hero section
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import pokeCompanionWebP from "../../assets/images/poke-companion.webp";
+import pokeCompanionTrimWebP from "../../assets/images/poke-companion-trim.webp";
 import pokeCompanion from "../../assets/images/poke-companion.png";
 import pokeCompanionTrim from "../../assets/images/poke-companion-trim.png";
 
@@ -16,19 +18,26 @@ const Hero = () => {
       <div
         className="hero-background"
         style={{
-          backgroundImage: `url(${pokeCompanionTrim})`,
+          backgroundImage: `url(${pokeCompanionTrimWebP})`,
         }}
       />
-      <img
-        src={pokeCompanion}
-        srcSet={`${pokeCompanionTrim} 300w, ${pokeCompanion} 600w`}
-        sizes="(max-width: 600px) 300px, 600px"
-        alt="poke-companion-sm"
-        className="hero-image"
-        onClick={handleImageClick}
-        loading="lazy"
-        style={{ cursor: "pointer" }}
-      />
+      <picture>
+        <source
+          srcSet={`${pokeCompanionTrimWebP} 300w, ${pokeCompanionWebP} 600w`}
+          sizes="(max-width: 600px) 300px, 600px"
+          type="image/webp"
+        />
+        <img
+          src={pokeCompanion}
+          srcSet={`${pokeCompanionTrim} 300w, ${pokeCompanion} 600w`}
+          sizes="(max-width: 600px) 300px, 600px"
+          alt="poke-companion-sm"
+          className="hero-image"
+          onClick={handleImageClick}
+          loading="lazy"
+          style={{ cursor: "pointer" }}
+        />
+      </picture>
     </div>
   );
 };
